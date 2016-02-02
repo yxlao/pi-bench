ef04c48f7fb97981947dbc0da14683a9f01a44db
Yixing Lao <linuxthink@gmail.com>
2016-02-02 02:11:58 -0800

    add ifdef header guard

--- cycles.h
+++ cycles.h
@@ -1,6 +1,10 @@
+#ifndef CYCLES_H
+#define CYCLES_H
 
-#define RESET           asm volatile("FMSTAT");asm volatile("mcr p15, 0, %0, c15, c12, 0" : : "r"(5));asm volatile("FMSTAT");
+#define RESET asm volatile("FMSTAT");asm volatile("mcr p15, 0, %0, c15, c12, 0" : : "r"(5));asm volatile("FMSTAT");
 #define GET_CCNT(value) asm volatile("FMSTAT");asm volatile("mrc p15, 0, %0, c15, c12, 1" : "=r"(value));asm volatile("FMSTAT");
-#define STD_ITER 1000
+#define STD_ITER 1
 unsigned long start_time;
 unsigned long end_time;
+
+#endif
