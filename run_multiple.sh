48507485a1aa1f79be36e60349589a9fdf4955cc
Yixing Lao <linuxthink@gmail.com>
2016-02-02 04:13:39 -0800

    add for loop 100 time in test

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 make
+make clean_txt
 
 for f in *.cc
 do
     f="${f%.*}"
     echo "#####"
     echo "running "$f
-    ./$f
+    for i in {1..100}
+    do
+        # ./$f | tee -a $f".txt"
+        ./$f >> $f".txt"
+    done
 done
