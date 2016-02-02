2454d576f7e370b3863817651bc51c27df75d6fb
Yunli Wang <yuw258@ucsd.edu>
2016-02-02 00:07:12 -0800

    seprate clear counter from init

--- raspbian-ccr/cycles.c
+++ raspbian-ccr/cycles.c
@@ -1,12 +1,15 @@
 static inline void ccnt_init (void) {
-  __asm__ volatile ("mcr p15, 0, %0, c15, c12, 0" : : "r" (4));
-  __asm__ volatile ("mcr p15, 0, %0, c15, c12, 0" : : "r" (1));
+  asm volatile ("mcr p15, 0, %0, c15, c12, 0" : : "r" (1));
+}
+
+static inline void ccnt_clr (void) {
+  asm volatile ("mcr p15, 0, %0, c15, c12, 0" : : "r" (5));
 }
 
 
 static inline unsigned ccnt_read (void)
 {
   unsigned cc;
-  __asm__ volatile ("mrc p15, 0, %0, c15, c12, 1":"=r" (cc));
+  asm volatile ("mrc p15, 0, %0, c15, c12, 1":"=r" (cc));
   return cc;
 }
