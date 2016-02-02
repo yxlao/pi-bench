2781913e9e2309c76dd66dc222c825a8a3d259be
jiw241 <jiw241@ucsd.edu>
2016-02-02 03:56:48 -0800

    compile with -pthread flag

---
+++ test_thread_create.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+// #include "raspbian-ccr/cycles.c"
+#include "cycles.h"
+// #include <cstdint>
+#include <pthread.h>
+using namespace std;
+
+void * RunThread(void * ) {
+  GET_CCNT(end_time);
+  pthread_exit(NULL);
+}
+
+int main() {
+    RESET;
+    pthread_t tid;
+    unsigned long total = 0;
+    for (int i = 0; i < STD_ITER; ++i) {
+        GET_CCNT(start_time);
+        pthread_create(&tid, NULL, RunThread, NULL);
+        pthread_join(tid, NULL); 
+        total += end_time - start_time;
+        // cout << start << "," << end << "," << end - start << endl;
+    }
+    cout << "average: " << 1. * total / STD_ITER << endl;
+    return 0;
+}
---
+++ test_thread_switch.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+// #include "raspbian-ccr/cycles.c"
+#include "cycles.h"
+// #include <cstdint>
+#include <pthread.h>
+#include <unistd.h>
+using namespace std;
+int fd[2];
+unsigned long start;
+void * RunThread(void * ) {
+  GET_CCNT(end_time);
+ // write(fd[1], (void*)&start, sizeof(unsigned long));
+  pthread_exit(NULL);
+}
+
+int main() {
+    RESET;
+    pthread_t tid;
+    unsigned long total = 0;
+    unsigned long total1 = 0;
+    for (int i = 0; i < STD_ITER; ++i) {
+       // pipe(fd);
+        pthread_create(&tid, NULL, RunThread, NULL);
+       // read(fd[0], &start_time, 1);
+        GET_CCNT(start_time);
+        pthread_join(tid, NULL); 
+       // GET_CCNT(end_time);
+        total += end_time - start_time;
+       // total1 += end_time - start;
+        // cout << start << "," << end << "," << end - start << endl;
+    }
+    cout << "average: " << 1. * total / STD_ITER <<" " << 1. * total / STD_ITER << endl;
+    return 0;
+}
