45ad22bbf0e45b4d699c0a73c173a6dec8fa4c61
jiw241 <jiw241@ucsd.edu>
2016-02-13 01:10:56 -0800

    add ram access test

---
+++ m1_raw_access.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include "utils.h"
+using namespace std;
+
+int main() {
+    int i, j, k, size = 1, read, ind;
+    time_t t;
+    unsigned long start, end;
+    srand((unsigned)time(&t));
+    for (i = 0; i < 20; i++) {
+        int * data = new int[size];
+        //initial data
+        for (j = 0; j < NUM_TRIAL; j++) {
+            unsigned long time_total = 0;
+            RESET_CCNT;
+            for (k = 0; k < NUM_ITER; k++) {
+                ind = rand() % size;
+                GET_CCNT(start);
+                read = data[ind];
+                GET_CCNT(end);
+                time_total += end - start;
+            }
+            if (j == 10) {
+                cout << "memory read time of size " << size << " is: " << 1. * time_total / NUM_ITER << endl;
+            }
+        }
+        size = size * 2;  
+    }
+    return 0;
+}
