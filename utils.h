967dc18773a2f0147a33e1cfc539c1bdb98b8603
Yixing Lao <linuxthink@gmail.com>
2016-02-12 21:00:17 -0800

    rename cycles.h

--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
-#ifndef CYCLES_H
-#define CYCLES_H
+#ifndef UTILS_H
+#define UTILS_H
 
 #define RESET asm volatile("FMSTAT");asm volatile("mcr p15, 0, %0, c15, c12, 0" : : "r"(5));asm volatile("FMSTAT");
 #define GET_CCNT(value) asm volatile("FMSTAT");asm volatile("mrc p15, 0, %0, c15, c12, 1" : "=r"(value));asm volatile("FMSTAT");
