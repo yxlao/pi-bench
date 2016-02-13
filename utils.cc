92a1463629dc56fcf217195f82843a537431c17a
Yixing Lao <linuxthink@gmail.com>
2016-02-13 03:00:38 -0800

    fix mean sd func, add print_stats func

--- utils.cc
+++ utils.cc
@@ -10,24 +10,3 @@ unsigned long time_trials[NUM_TRIAL];
 unsigned long time_start;
 unsigned long time_end;
 unsigned long time_total = 0;
-
-template <class T> float get_mean(T* data, int n) {
-    // mean
-    float mean = 0.0;
-    for (int i = 0; i < n; ++i) {
-        mean += float(data[i]);
-    }
-    mean = mean / n;
-    return mean;
-}
-
-template <class T> float get_std(T* data, int n) {
-    // mean
-    float mean = get_mean(data, n);
-    // std
-    float sum_deviation = 0.0;
-    for (int i = 0; i < n; ++i) {
-        sum_deviation += (float(data[i]) - mean) * (float(data[i]) - mean);
-    }
-    return sqrt(sum_deviation / n);
-}
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <math.h>
+
 // reset counter
 #define RESET_CCNT\
     asm volatile("FMSTAT");\
@@ -26,8 +28,36 @@ extern unsigned long time_start;
 extern unsigned long time_end;
 extern unsigned long time_total;
 
-// utilities functions
-template <class T> float get_mean(T* data, int n);
-template <class T> float get_std(T* data, int n);
+// util template functions
+template <class T> float get_mean(T* data, int n) {
+    // mean
+    float mean = 0.0;
+    for (int i = 0; i < n; ++i) {
+        mean += float(data[i]);
+    }
+    mean = mean / n;
+    return mean;
+}
+
+template <class T> float get_sd(T* data, int n) {
+    // mean
+    float mean = get_mean(data, n);
+    // std
+    float sum_deviation = 0.0;
+    for (int i = 0; i < n; ++i) {
+        sum_deviation += (float(data[i]) - mean) * (float(data[i]) - mean);
+    }
+    return sqrt(sum_deviation / n);
+}
+
+template <class T> void print_stats(T* data, int n) {
+    float mean = get_mean(data, n);
+    float sd = get_sd(data, n);
+    std::cout << "num_trial: " << NUM_TRIAL << std::endl
+              << "nnum_iter: " << NUM_ITER << std::endl
+              << "mean_op: " << mean / float(NUM_ITER) << std::endl
+              << "mean_trial: " << mean << std::endl
+              << "std_trail: " << sd << std::endl;
+}
 
 #endif
