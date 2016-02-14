c9810fbc7d5ac12a5919c35e8989dd9e8f4942db
Yixing Lao <linuxthink@gmail.com>
2016-02-13 19:22:14 -0800

    rename num_iter to num_trial in the second experiment

--- c2_proc_call.cc
+++ c2_proc_call.cc
@@ -37,79 +37,77 @@ void fun7(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6,
 }
 
 int main() {
-    // ccnt_clr();
     RESET_CCNT;
 
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun0();
         time_total += time_end - time_start;
-        //cout << start - start << "," << end << "," << end - start << endl;
     }
-    cout << "procedure call 0: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 0: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun1(0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 1: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 1: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun2(0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 2: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 2: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun3(0, 0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 3: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 3: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun4(0, 0, 0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 4: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 4: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun5(0, 0, 0, 0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 5: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 5: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun6(0, 0, 0, 0, 0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 6: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 6: " << 1. * time_total / NUM_TRIAL << endl;
 
     RESET_CCNT;
     time_total = 0;
-    for (int i = 0; i < NUM_ITER; ++i) {
+    for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         fun7(0, 0, 0, 0, 0, 0, 0);
         time_total += time_end - time_start;
     }
-    cout << "procedure call 7: " << 1. * time_total / NUM_ITER << endl;
+    cout << "procedure call 7: " << 1. * time_total / NUM_TRIAL << endl;
 
     return 0;
 }
