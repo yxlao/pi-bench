6a5449d2fd298164eeb1d9eb969717422be15ce1
Yixing Lao <linuxthink@gmail.com>
2016-02-14 00:50:36 -0800

    remove ; in asm

--- utils.h
+++ utils.h
@@ -8,13 +8,13 @@
 #define RESET_CCNT\
     asm volatile("FMSTAT");\
     asm volatile("mcr p15, 0, %0, c15, c12, 0" : : "r"(5));\
-    asm volatile("FMSTAT");
+    asm volatile("FMSTAT")
 
 // get counter
 #define GET_CCNT(value)\
     asm volatile("FMSTAT");\
     asm volatile("mrc p15, 0, %0, c15, c12, 1" : "=r"(value));\
-    asm volatile("FMSTAT");
+    asm volatile("FMSTAT")
 
 // experiment repetitions
 #define NUM_TRIAL  1000
