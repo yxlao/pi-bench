655f0b2f09d43c33301c665aa6fcae963f6bcc4c
Yixing Lao <linuxthink@gmail.com>
2016-02-13 21:04:26 -0800

    add overhead per measurement vs per operation

--- c1_measurement.cc
+++ c1_measurement.cc
@@ -2,23 +2,41 @@
 #include "utils.h"
 using namespace std;
 
+/*
+We define two types of measurement overhead:
+Type 1: overhead per measurement
+t_loop * NUM_ITER + t_readcycle
+Type 2: overhead per operation
+(t_loop * NUM_ITER + t_readcycle) / (NUM_ITER * NUM_UNROLL)
+*/
 int main() {
+    // overhead per measurement
     RESET_CCNT;
     for (int i = 0; i < NUM_TRIAL; ++i) {
         GET_CCNT(time_start);
         for (int j = 0; j < NUM_ITER; ++j) {
         }
-        for (int j = 0; j < NUM_ITER; ++j) {
-        }
-        for (int j = 0; j < NUM_ITER; ++j) {
-        }
-        for (int j = 0; j < NUM_ITER; ++j) {
-        }
+        GET_CCNT(time_end);
+        time_trials[i] = time_end - time_start;
+    }
+    cout << "## Overhead per measurement:" << endl;
+    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
+
+    // overhead per operation
+    RESET_CCNT;
+    for (int i = 0; i < NUM_TRIAL; ++i) {
+        GET_CCNT(time_start);
         for (int j = 0; j < NUM_ITER; ++j) {
         }
         GET_CCNT(time_end);
         time_trials[i] = time_end - time_start;
     }
-    print_all_stats(time_trials, NUM_TRIAL, NUM_ITER, NUM_UNROLL);
+    for (int i = 0; i < NUM_TRIAL; ++i) {
+        time_trials[i] = (unsigned long) ((float)time_trials[i] /
+                                          (float)NUM_ITER /
+                                          (float)NUM_UNROLL);
+    }
+    cout << "Overhead per operation:" << endl;
+    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
     return 0;
 }
