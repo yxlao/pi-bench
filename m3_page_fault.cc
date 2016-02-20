950a9aeea3c6570780785f93d3443ef77d7d10eb
Yunli Wang <yuw258@ucsd.edu>
2016-02-19 21:03:33 -0800

    clearing cache does not do anything

--- m3_page_fault.cc
+++ m3_page_fault.cc
@@ -2,31 +2,42 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h> // for sync, write, and close
 
 
 int main(int argc, char *argv[]){
-    int x;
-    char * data;
     int fd = open("/home/pi/page_file.img", O_RDONLY);
     struct stat s;
     fstat (fd, & s);
     size_t size = s.st_size;
     std::cout << "size of file: " << size << std::endl;
-    data = (char*) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    char* data = (char*) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    int x;
 
-    std::cout << "number of iterations: " << NUM_ITER << std::endl;
+    std::cout << "address of data: " << (unsigned long) data << std::endl;
     for (int i = 0; i < NUM_TRIAL; ++i) {
         RESET_CCNT;
         GET_CCNT(time_start);
         //Loop on mem size
-        for (unsigned j = 0, i1; i1 < NUM_ITER; j += 1048576, ++i1 ){
+        for (unsigned j = 0, i1; i1 < 1; j += 40960, ++i1 ){
             x += data[j];
         }
         GET_CCNT(time_end);
+
+        int fd2;
+        char* data = "3";
+
+        sync();
+        fd2 = open("/proc/sys/vm/drop_caches", O_WRONLY);
+        write(fd2, data, sizeof(char));
+        close(fd2);
+
         time_trials[i] = time_end - time_start;
+        if (i < 10)
+            std::cout << time_trials[i] << std::endl;
     }
     std::cout << "x = " << x << std::endl;
     std::cout << "## Page fault " << std::endl;
-    print_all_stats(time_trials, NUM_TRIAL, NUM_ITER, 1);
+    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
     return 0;
 }
