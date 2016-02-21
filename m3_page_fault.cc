33c11c090d5c76fc238934b50d4da38508f22331
Yixing Lao <linuxthink@gmail.com>
2016-02-20 17:14:38 -0800

    randomized page access

--- m3_page_fault.cc
+++ m3_page_fault.cc
@@ -2,42 +2,51 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include "utils.h"
 #include <unistd.h> // for sync, write, and close
 
 
 int main(int argc, char *argv[]){
+    // random number
+    time_t t;
+    srand((unsigned)time(&t));
+
+    // open file and test size
     int fd = open("/home/pi/page_file.img", O_RDONLY);
     struct stat s;
     fstat (fd, & s);
     size_t size = s.st_size;
     std::cout << "size of file: " << size << std::endl;
     char* data = (char*) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    int x;
 
     std::cout << "address of data: " << (unsigned long) data << std::endl;
-    for (int i = 0; i < NUM_TRIAL; ++i) {
+
+    unsigned addr;
+    int temp;
+
+    for (int i = 0; i < 64; ++i) {
+        addr = (unsigned) ((float)rand() / (float)RAND_MAX * (float) size);
+
         RESET_CCNT;
         GET_CCNT(time_start);
-        //Loop on mem size
-        for (unsigned j = 0, i1; i1 < 1; j += 40960, ++i1 ){
-            x += data[j];
-        }
+        temp += data[addr];
         GET_CCNT(time_end);
 
-        int fd2;
-        char* data = "3";
-
+        int fdx;
+        char* datax = "3";
         sync();
-        fd2 = open("/proc/sys/vm/drop_caches", O_WRONLY);
-        write(fd2, data, sizeof(char));
-        close(fd2);
+        fdx = open("/proc/sys/vm/drop_caches", O_WRONLY);
+        write(fdx, datax, sizeof(char));
+        close(fdx);
 
         time_trials[i] = time_end - time_start;
-        if (i < 10)
-            std::cout << time_trials[i] << std::endl;
+        std::cout << time_trials[i] << std::endl;
     }
-    std::cout << "x = " << x << std::endl;
+    close(fd);
+    std::cout << "x = " << temp << std::endl;
     std::cout << "## Page fault " << std::endl;
-    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
+    print_all_stats(time_trials, 64, 1, 1);
     return 0;
 }
