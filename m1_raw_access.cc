d8dc4540726df667962b4431dc3c855d2a36b28b
Yixing Lao <linuxthink@gmail.com>
2016-02-23 05:32:32 -0800

    flush write at each array size

--- m1_raw_access.cc
+++ m1_raw_access.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 #include "utils.h"
 
 // experiment repetitions
@@ -9,7 +10,7 @@
 unsigned long time_trials[NUM_TRIAL];
 
 // maximum size of integer array
-#define MAX_INT_ARRAY 67108864 // 64M ints, 256M bytes
+#define MAX_INT_ARRAY_SIZE 67108864 // 2^26=64M ints, 256M bytes
 
 // deprecated random index
 // #include <time.h>
@@ -17,29 +18,43 @@ unsigned long time_trials[NUM_TRIAL];
 // srand((unsigned)time(&t));
 // ind = (unsigned int)((float)rand() / (float)RAND_MAX * size);
 
-int main() {
-    int size = 1, temp;
-    unsigned long ind;
-    int* data = new int[MAX_INT_ARRAY];
-    for (int i = 0; i < MAX_INT_ARRAY; i++) {
+void flush_write(int* data, int max_ind) {
+    for (int i = 0; i < max_ind; i++) {
         data[i] = i % 10;
     }
+    return;
+}
 
-    for (int s = 0; s < 30; s++) {
-        RESET_CCNT;
+int main() {
+    // allocate data
+    int* data = new int[MAX_INT_ARRAY_SIZE];
+    flush_write(data, MAX_INT_ARRAY_SIZE);
+
+    int temp, real_ind;
+    unsigned long ind;
+    int size = 1;
+
+    for (int step = 0; step < 27; step++) {
+        // init
+        size = (int) pow(2, step);
         ind = 0;
+        flush_write(data, size);
+        RESET_CCNT;
+        // run
         for (int i = 0; i < NUM_TRIAL; i++) {
-            int real_ind = ind % size;
+            real_ind  = ind % size;
             GET_CCNT(time_start);
             temp = data[real_ind];
             GET_CCNT(time_end);
             time_trials[i] = time_end - time_start;
             ind += 8;
         }
-        std::cout << s << ": " << size << std::endl;
+        // finishing
+        std::cout << step << ": " << size << std::endl;
         print_trimmed_mean_std(time_trials, NUM_TRIAL, 1, 1);
-        size = size * 2;
     }
+
+    // clen-ups
     (void) temp;
     delete data;
     return 0;
