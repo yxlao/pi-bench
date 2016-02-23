96a44b734a35e499d0a1f7277197fc0a62367033
Yixing Lao <linuxthink@gmail.com>
2016-02-23 07:20:48 -0800

    32M no unroll experiment

--- m2_raw_read_bandwidth.cc
+++ m2_raw_read_bandwidth.cc
@@ -4,36 +4,35 @@
 #include "utils.h"
 
 // experiment repetitions
-#define NUM_TRIAL  50
+#define NUM_TRIAL 100
 unsigned long time_trials[NUM_TRIAL];
 using namespace std;
 
-// #define MAX_CHAR_ARRAY_SIZE 268435456 // 256M bytes
-#define MAX_CHAR_ARRAY_SIZE 16777216 // 33554432 // 32M bytes
-#define CACHE_FLUSH_SIZE 4194304 // 4M bytes, flush L1, L2 cache
+#define MAX_INT_ARRAY_SIZE 16777216 / 2 // 32M Bytes
+#define CACHE_FLUSH_SIZE 4194304 / 4
 
-void flush_write(char* data, int max_ind) {
-     for (int i = 0; i < max_ind; i++) {
-          data[i] = 'a' + i % ('z' - 'a');
-     }
-     return;
+void flush_write(int* data, int max_ind) {
+    for (int i = 0; i < max_ind; i++) {
+        data[i] = i;
+    }
+    return;
 }
 
-void flush_read(char* data, int max_ind) {
-     char temp;
-     for (int i = 0; i < max_ind; i++) {
-          temp = data[i];
-     }
-     (void) temp;
-     return;
+void flush_read(int* data, int max_ind) {
+    int temp;
+    for (int i = 0; i < max_ind; i++) {
+        temp = data[i];
+    }
+    (void) temp;
+    return;
 }
 
 int main() {
      // init data
-     char temp;
-     char* data = new char[MAX_CHAR_ARRAY_SIZE];
-     char* cache_data = new char[CACHE_FLUSH_SIZE];
-     flush_write(data, MAX_CHAR_ARRAY_SIZE);
+     int temp;
+     int* data = new int[MAX_INT_ARRAY_SIZE];
+     int* cache_data = new int[CACHE_FLUSH_SIZE];
+     flush_write(data, MAX_INT_ARRAY_SIZE);
      flush_write(cache_data, CACHE_FLUSH_SIZE);
 
      // experiments
@@ -42,23 +41,8 @@ int main() {
           flush_read(cache_data, CACHE_FLUSH_SIZE);
           RESET_CCNT;
           GET_CCNT(time_start);
-          for (int j = 0; j < MAX_CHAR_ARRAY_SIZE; j += 1) {
+          for (int j = 0; j < MAX_INT_ARRAY_SIZE; j += 4) {
                temp = data[j];
-               // temp = data[j + 32];
-               // temp = data[j + 64];
-               // temp = data[j + 96];
-               // temp = data[j + 128];
-               // temp = data[j + 160];
-               // temp = data[j + 192];
-               // temp = data[j + 224];
-               // temp = data[j + 256];
-               // temp = data[j + 288];
-               // temp = data[j + 320];
-               // temp = data[j + 352];
-               // temp = data[j + 384];
-               // temp = data[j + 416];
-               // temp = data[j + 448];
-               // temp = data[j + 480];
           }
           GET_CCNT(time_end);
           time_trials[i] = time_end - time_start;
--- m2_raw_write_bandwidth.cc
+++ m2_raw_write_bandwidth.cc
@@ -4,23 +4,22 @@
 #include "utils.h"
 
 // experiment repetitions
-#define NUM_TRIAL  50
+#define NUM_TRIAL 100
 unsigned long time_trials[NUM_TRIAL];
 using namespace std;
 
-// #define MAX_CHAR_ARRAY_SIZE 268435456 // 256M bytes
-#define MAX_CHAR_ARRAY_SIZE 16777216 // 33554432 // 32M bytes
-#define CACHE_FLUSH_SIZE 4194304 // 4M bytes, flush L1, L2 cache
+#define MAX_INT_ARRAY_SIZE 16777216 / 2 // 32M Bytes
+#define CACHE_FLUSH_SIZE 4194304 / 4
 
-void flush_write(char* data, int max_ind) {
+void flush_write(int* data, int max_ind) {
     for (int i = 0; i < max_ind; i++) {
-        data[i] = 'a' + i % ('z' - 'a');
+        data[i] = i;
     }
     return;
 }
 
-void flush_read(char* data, int max_ind) {
-    char temp;
+void flush_read(int* data, int max_ind) {
+    int temp;
     for (int i = 0; i < max_ind; i++) {
         temp = data[i];
     }
@@ -30,10 +29,10 @@ void flush_read(char* data, int max_ind) {
 
 int main() {
     // init data
-    char temp;
-    char* data = new char[MAX_CHAR_ARRAY_SIZE];
-    char* cache_data = new char[CACHE_FLUSH_SIZE];
-    flush_write(data, MAX_CHAR_ARRAY_SIZE);
+    int temp;
+    int* data = new int[MAX_INT_ARRAY_SIZE];
+    int* cache_data = new int[CACHE_FLUSH_SIZE];
+    flush_write(data, MAX_INT_ARRAY_SIZE);
     flush_write(cache_data, CACHE_FLUSH_SIZE);
 
     // experiments
@@ -42,23 +41,8 @@ int main() {
         flush_read(cache_data, CACHE_FLUSH_SIZE);
         RESET_CCNT;
         GET_CCNT(time_start);
-        for (int j = 0; j < MAX_CHAR_ARRAY_SIZE; j += 1) {
-            data[j] = 'a';
-            // data[j + 32] = 'a';
-            // data[j + 64] = 'a';
-            // data[j + 96] = 'a';
-            // data[j + 128] = 'a';
-            // data[j + 160] = 'a';
-            // data[j + 192] = 'a';
-            // data[j + 224] = 'a';
-            // data[j + 256] = 'a';
-            // data[j + 288] = 'a';
-            // data[j + 320] = 'a';
-            // data[j + 352] = 'a';
-            // data[j + 384] = 'a';
-            // data[j + 416] = 'a';
-            // data[j + 448] = 'a';
-            // data[j + 480] = 'a';
+        for (int j = 0; j < MAX_INT_ARRAY_SIZE; j += 4) {
+            data[j] = j;
         }
         GET_CCNT(time_end);
         time_trials[i] = time_end - time_start;
