71c57964c500cc7ff554904c484f68b19f876d48
jiw241 <jiw241@ucsd.edu>
2016-02-22 19:14:26 -0800

    modify read and write bandwidth

--- m2_raw_read_bandwidth.cc
+++ m2_raw_read_bandwidth.cc
@@ -13,16 +13,31 @@ using namespace std;
 int main() {
     int i, j;
     char temp;
-    char * data = new char[4194304];//4MByte = 4M Characters = 4 * 1024 * 1024 chars = 4194304
-    for (int i = 0; i < 4194304; i++) {
+    char * data = new char[262144];//256KByte = 256 * 1024 Characters = 262144
+    for (int i = 0; i < 262144; i++) {
         data[i] = 'a' + i % ('z' - 'a');
     }
     //read test
     for (i = 0; i < NUM_TRIAL; i++) {
         RESET_CCNT;
         GET_CCNT(time_start);
-        for (j = 0; j < 4194304; j++) {
-            temp = data[j];
+        for (j = 0; j < 261664; j += 512) {
+           temp = data[j];
+           temp = data[j + 32];
+           temp = data[j + 64];
+           temp = data[j + 96];
+           temp = data[j + 128];
+           temp = data[j + 160];
+           temp = data[j + 192];
+           temp = data[j + 224];
+           temp = data[j + 256];
+           temp = data[j + 288];
+           temp = data[j + 320];
+           temp = data[j + 352];
+           temp = data[j + 384];
+           temp = data[j + 416];
+           temp = data[j + 448];
+           temp = data[j + 480];
         }
         GET_CCNT(time_end);
         time_trials[i] = time_end - time_start;
--- m2_raw_write_bandwidth.cc
+++ m2_raw_write_bandwidth.cc
@@ -2,31 +2,48 @@
 #include <time.h>
 #include <stdlib.h>
 #include "utils.h"
-using namespace std;
 
 // experiment repetitions
 #define NUM_TRIAL  10000
 #define NUM_ITER   200
 #define NUM_UNROLL 5
 unsigned long time_trials[NUM_TRIAL];
+using namespace std;
 
 int main() {
     int i, j;
-    char * data = new char[4194304];//4MByte = 4194304 Chars
-    for (int i = 0; i < 4194304; i++) {
+    char temp;
+    char * data = new char[262144];//256KByte = 256 * 1024 Characters = 262144
+    for (int i = 0; i < 262144; i++) {
         data[i] = 'a' + i % ('z' - 'a');
     }
     //write test
     for (i = 0; i < NUM_TRIAL; i++) {
         RESET_CCNT;
         GET_CCNT(time_start);
-        for (j = 0; j < 4194304; j++) {
-            data[j] = 'a';
+        for (j = 0; j < 261664; j += 512) {
+           data[j] = 'a';
+           data[j + 32] = 'a';
+           data[j + 64] = 'a';
+           data[j + 96] = 'a';
+           data[j + 128] = 'a';
+           data[j + 160] = 'a';
+           data[j + 192] = 'a';
+           data[j + 224] = 'a';
+           data[j + 256] = 'a';
+           data[j + 288] = 'a';
+           data[j + 320] = 'a';
+           data[j + 352] = 'a';
+           data[j + 384] = 'a';
+           data[j + 416] = 'a';
+           data[j + 448] = 'a';
+           data[j + 480] = 'a';
         }
         GET_CCNT(time_end);
         time_trials[i] = time_end - time_start;
     }
     print_trimmed_mean_std(time_trials, NUM_TRIAL, 1, 1);
+    (void) temp;  // make g++ "unused variable" go away
     delete data;
     return 0;
 }
