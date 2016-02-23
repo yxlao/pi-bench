0145069b08e07a0e997ef5d4946e515933a933e7
Yixing Lao <linuxthink@gmail.com>
2016-02-23 05:01:38 -0800

    fix style

--- m1_raw_access.cc
+++ m1_raw_access.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <time.h>
 #include <stdlib.h>
 #include "utils.h"
 
@@ -9,36 +8,36 @@
 #define NUM_UNROLL 5
 unsigned long time_trials[NUM_TRIAL];
 
-int main() {
-  int size = 1, temp;
-  unsigned long ind;
-  time_t t;
-  srand((unsigned)time(&t));
-  int* data = new int[67108864];
-  for (int i = 0; i < 67108864; i++) {
-     data[i] = i % 10;
-  }
-  //unsigned long * my_time_trials = new unsigned long[NUM_TRIAL * 100];
+// deprecated random index
+// #include <time.h>
+// time_t t;
+// srand((unsigned)time(&t));
+// ind = (unsigned int)((float)rand() / (float)RAND_MAX * size);
 
-  for (int s = 0; s < 30; s++) {
-    RESET_CCNT;
-    ind = 0;
-    for (int i = 0; i < NUM_TRIAL; i++) {
-      //ind = (unsigned int)((float)rand() / (float)RAND_MAX * size);
-      int real_ind = ind % size;
-      GET_CCNT(time_start);
-      temp = data[real_ind];
-      GET_CCNT(time_end);
-    //  data[ind] = temp;
-      time_trials[i] = time_end - time_start;
-      ind += 8;
+int main() {
+    int size = 1, temp;
+    unsigned long ind;
+    int* data = new int[67108864];
+    for (int i = 0; i < 67108864; i++) {
+        data[i] = i % 10;
     }
-    std::cout << s << ": " << size << std::endl;
-    print_trimmed_mean_std(time_trials, NUM_TRIAL, 1, 1);
-    size = size * 2;
-  }
-  (void) temp;
-  delete data;
-  // delete[] my_time_trials;
-  return 0;
+
+    for (int s = 0; s < 30; s++) {
+        RESET_CCNT;
+        ind = 0;
+        for (int i = 0; i < NUM_TRIAL; i++) {
+            int real_ind = ind % size;
+            GET_CCNT(time_start);
+            temp = data[real_ind];
+            GET_CCNT(time_end);
+            time_trials[i] = time_end - time_start;
+            ind += 8;
+        }
+        std::cout << s << ": " << size << std::endl;
+        print_trimmed_mean_std(time_trials, NUM_TRIAL, 1, 1);
+        size = size * 2;
+    }
+    (void) temp;
+    delete data;
+    return 0;
 }
