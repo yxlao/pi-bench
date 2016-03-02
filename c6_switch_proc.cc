07784ce8afd308273d02bde18487c7050107bc26
Yixing Lao <linuxthink@gmail.com>
2016-03-02 03:53:20 -0800

    rename to pipe fd to c2p

--- c6_switch_proc.cc
+++ c6_switch_proc.cc
@@ -12,8 +12,8 @@ unsigned long time_trials[NUM_TRIAL];
 
 int main() {
     // pipes
-    int fd[2];
-    pipe(fd);
+    int c2p[2];
+    pipe(c2p);
 
     // pid_t
     pid_t cpid;
@@ -22,14 +22,16 @@ int main() {
         RESET_CCNT;
         // fork
         cpid = fork();
-        // parent
         if (cpid != 0) {
+            // parent
             GET_CCNT(time_start);
             wait(NULL);
-            read(fd[0], (void*)&time_end, sizeof(unsigned long));
+            read(c2p[0], (void*)&time_end, sizeof(unsigned long));
+            // std::cout << "parent read 0" << std::endl;
         } else {
+            // children
             GET_CCNT(time_end);
-            write(fd[1], (void*)&time_end, sizeof(unsigned long));
+            write(c2p[1], (void*)&time_end, sizeof(unsigned long));
             exit(1);
         }
         if (time_end > time_start) {
