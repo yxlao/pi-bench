da4229cfdce32d4d2b98d7f011c852fc4211b73e
jiw241 <jiw241@ucsd.edu>
2016-03-05 15:49:24 -0800

    modify m4_file_read, still has error

--- m4_file_read.cc
+++ m4_file_read.cc
@@ -29,31 +29,32 @@ int main() {
      //define variables
      long long int size =4096;
      
-     int counter = 0;   
+     //int counter = 0;   
  
      char filename[128];
      char * buffer = new char[BLOCK_SIZE];     
 
      struct timeval start, stop;
 
-     ifstream file;
-     file.rdbuf() -> pubsetbuf(0, 0);//disable read buffer
-/*     //sequential access
-     for (int i = 0; i < 19; i++) {
+     ifstream file1, file2;
+     file1.rdbuf() -> pubsetbuf(0, 0);//disable read buffer
+     file2.rdbuf() -> pubsetbuf(0, 0);
+     //sequential access
+     for (int i = 0; i < 18; i++) {
        
        sprintf (filename, "/home/pi/temp_%lld", size);
     
-       file.open(filename, ios::in); 
+       file1.open(filename, ios::in); 
 
-       file.seekg(0, ios::beg);
+       file1.seekg(0, ios::beg);
 
        gettimeofday(&start, NULL);
 
        //counter = 0;	
        for (long long int k = 0; k < size; k += BLOCK_SIZE) {
-           //file.seekg(k, ios::beg);
-           file.read(buffer, BLOCK_SIZE);
-           if (!file) {
+           //file1.seekg(k, ios::beg);
+           file1.read(buffer, BLOCK_SIZE);
+           if (!file1) {
               cout << k <<  " seq read end early " << size <<endl;
               break;
            }
@@ -64,21 +65,18 @@ int main() {
 	    
        cout << "size: " << size << " seq: " << ((stop.tv_sec - start.tv_sec) * 1000000L) + stop.tv_usec - start.tv_usec << endl;
 	    
-       file.close(); 
+       file1.close(); 
       
        size *= 2;       
       
      }//end of sequential access 
-*/
+
 
     //reset parameters
     size = 4096;
-    file.clear();
-    ifstream file2;    
 
-    file2.clear();
     //random access
-    for (int i = 0; i < 19; i++) {
+    for (int i = 0; i < 18; i++) {
  
        sprintf (filename, "home/pi/temp_%lld", size);
       
@@ -93,18 +91,18 @@ int main() {
        }
   
        file2.seekg(0, ios::beg);
-       if (!file2) cout << "file2 fila at the very beginning" << endl;
+       //if (!file2) cout << "file2 fail at the very beginning" << endl;
        gettimeofday(&start, NULL);
        //counter = 0;
        for (long long int k = 0; k < size; k += BLOCK_SIZE) {
-           //file2.seekg(randInd[k / BLOCK_SIZE], ios::beg);
-           file2.seekg(k, ios::beg);
-           cout << "randInd" << randInd[k / BLOCK_SIZE] << endl;
+           file2.seekg(randInd[k / BLOCK_SIZE], ios::beg);
+         //  file2.seekg(k, ios::beg);
+         //  cout << "randInd" << randInd[k / BLOCK_SIZE] << endl;
            file2.read(buffer, BLOCK_SIZE);
-           if (!file2){
-              cout << "rand read end early" << endl;
-              break;
-           }
+         //  if (!file2){
+           //   cout << "rand read end early" << endl;
+            //  break;
+         //  }
            //counter ++;
        }
 
