febed56e3661539602140a51f5ae8bb388e45986
Yunli Wang <yuw258@ucsd.edu>
2016-03-05 18:59:27 -0800

    Removing unnecessary spaces

--- f2_file_read.cc
+++ f2_file_read.cc
@@ -18,10 +18,10 @@ using namespace std;
 int main() {
      //define variables
      long long int size =4096;
-     srand((unsigned int)time(NULL));    
+     srand((unsigned int)time(NULL));
 
      char filename[128];
-     char * buffer = new char[BLOCK_SIZE];     
+     char * buffer = new char[BLOCK_SIZE];
 
      struct timeval start, stop;
 
@@ -29,17 +29,17 @@ int main() {
 
      //sequential access
      for (int i = 0; i < 18; i++) {
-       
+
        sprintf (filename, "/home/pi/temp_%lld", size);
-    
-       file1 = open(filename, O_RDONLY | O_DIRECT); 
+
+       file1 = open(filename, O_RDONLY | O_DIRECT);
        if (file1 == -1) {
            cout << "seq open file failed, size: " << size <<  endl;
        }
 
        gettimeofday(&start, NULL);
 
-       //counter = 0;	
+       //counter = 0;
        for (long long int k = 0; k < size; k += BLOCK_SIZE) {
            ssize_t byte = read(file1, buffer, BLOCK_SIZE);
            if (byte <= 0) {
@@ -47,16 +47,16 @@ int main() {
               break;
            }
        }
-  
+
        gettimeofday(&stop, NULL);
-	    
+
        cout << "size: " << size << " seq: " << ((stop.tv_sec - start.tv_sec) * 1000000L) + stop.tv_usec - start.tv_usec << endl;
-	    
-       close(file1); 
-      
-       size *= 2;       
-      
-     }//end of sequential access 
+
+       close(file1);
+
+       size *= 2;
+
+     }//end of sequential access
 
 
     //reset parameters
@@ -64,23 +64,23 @@ int main() {
 
     //random access
     for (int i = 0; i < 18; i++) {
- 
+
        sprintf (filename, "home/pi/temp_%lld", size);
-      
+
        file2 = open(filename, O_RDONLY | O_DIRECT);
        if (file2 == -1) {
            cout << "rand open file failed, size: " << size << endl;
        }
        int block_num = ceil((double)size / (double)BLOCK_SIZE);
-       
+
        long long int * randInd = new long long int[block_num];
-  
+
        for (int i = 0; i < block_num; i++) {
            randInd[i] = rand() % block_num * BLOCK_SIZE;
        }
-  
+
        gettimeofday(&start, NULL);
-       
+
        //counter = 0;
        for (long long int k = 0; k < size; k += BLOCK_SIZE) {
            lseek(file2, randInd[k / BLOCK_SIZE], SEEK_SET);
@@ -91,14 +91,14 @@ int main() {
            }
        }
 
-       gettimeofday(&stop, NULL); 
-  
+       gettimeofday(&stop, NULL);
+
        cout << "size: " << size << " ran: " << ((stop.tv_sec - start.tv_sec) * 1000000L) + stop.tv_usec - start.tv_usec << endl;
 
        close(file2);
-        
+
        size *= 2;
     }//end of random access
-     
+
      return 0;
 }
