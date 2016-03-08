a146b7e3e2f674717db5f9219e2fdfab2fed096a
Yunli Wang <yuw258@ucsd.edu>
2016-03-08 00:14:01 -0800

    Contention read speed in right order of magnitude but  not consitent

--- f4_contention_thread.cc
+++ f4_contention_thread.cc
@@ -13,10 +13,11 @@
 #define MAX_THREAD 32
 unsigned long time_trials[NUM_TRIAL];
 const long long block_size = 4096;
-
+const long long buffer_size = 8388608; // 8M * 32 = 256M, manageable
+const long long max_file_size = 16777216;
 pthread_barrier_t barrier;
 struct thread_params {
-  long long overhead_block;
+  unsigned long long overhead_block;
   int i;
 };
 
@@ -24,7 +25,7 @@ void* file_read_thread(void* void_params) {
   thread_params* params = (thread_params*) void_params;
   int page_size = sysconf(_SC_PAGESIZE);
   long* data;
-  long long time1, time2;
+  unsigned long time1, time2;
 
   char filename[50];
   sprintf(filename, "/home/pi/contention_%d", params->i);
@@ -32,19 +33,26 @@ void* file_read_thread(void* void_params) {
   struct stat s;
   fstat (fd, & s);
   size_t file_size = s.st_size;
+  if (file_size > max_file_size) {
+    file_size = max_file_size;
+  }
 
-  posix_memalign((void**) &data, page_size, file_size);
+  posix_memalign((void**) &data, page_size, buffer_size);
 
   pthread_barrier_wait(&barrier);
 
   RESET_CCNT;
   GET_CCNT(time1);
-  read(fd, data, file_size);
+  for (size_t i = 0; i < file_size; i += buffer_size) {
+    read(fd, data, buffer_size);
+  }
   GET_CCNT(time2);
   close(fd);
   free(data);
 
-  params->overhead_block = (time2 - time1) * block_size / file_size;
+  // unsigned long time_block = (time2 - time1) / file_size * block_size;
+  // std::cout << params->i << ": (" << time2 << ", " << time1 << ") -> " <<time_block << std::endl;
+  params->overhead_block = (time2 - time1) / file_size * block_size;
 
   return NULL;
 }
@@ -71,7 +79,7 @@ int main() {
     free( threads );
     free( arr_params );
 
-    std::cout << count << " threads: cycles per block: " << overhead / count << std::endl;
+    std::cout << count << " threads: cycles per block: " << overhead / count << " speed: " << block_size * count * 7e8 / overhead / 1024 << "kB/s"<< std::endl;
   }
   return 0;
 }
