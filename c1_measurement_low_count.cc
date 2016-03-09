e571c88f5b2558291e077cea1c6400d443f7264d
jiw241 <jiw241@ucsd.edu>
2016-03-08 23:09:01 -0800

    add overhead measurement test for GET_LOW_CCNT

---
+++ c1_measurement_low_count.cc
@@ -0,0 +1,41 @@
+#include "utils.h"
+
+// experiment repetitions
+#define NUM_TRIAL  10000
+#define NUM_ITER   200
+#define NUM_UNROLL 5
+unsigned long time_trials[NUM_TRIAL];
+
+/*
+We define two types of measurement overhead:
+Type 1: overhead per measurement
+t_loop * NUM_ITER + t_readcycle
+Type 2: overhead per operation
+(t_loop * NUM_ITER + t_readcycle) / (NUM_ITER * NUM_UNROLL)
+*/
+int main() {
+    // cycle register read overhead
+    RESET_CCNT;
+    for (int i = 0; i < NUM_TRIAL; ++i) {
+        GET_LOW_CCNT(time_start);
+        GET_LOW_CCNT(time_end);
+        time_trials[i] = (time_end - time_start) * 64;
+    }
+    std::cout << "##### Register read overhead:" << std::endl;
+    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
+
+    // register read overhead + loop overhead
+    RESET_CCNT;
+    for (int i = 0; i < NUM_TRIAL; ++i) {
+        GET_LOW_CCNT(time_start);
+        for (int j = 0; j < NUM_ITER; ++j) {
+            // do operation NUM_UNROLL times
+        }
+        GET_LOW_CCNT(time_end);
+        time_trials[i] = (time_end - time_start) * 64;
+    }
+    std::cout << "##### Loop + register read overhead:" << std::endl;
+    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
+
+    return 0;
+}
