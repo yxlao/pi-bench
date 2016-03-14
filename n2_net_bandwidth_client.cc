bca38a8416ded9d7f42794b14df483f49718903c
Yixing Lao <linuxthink@gmail.com>
2016-03-14 06:04:46 -0700

    add pings output

--- n2_net_bandwidth_client.cc
+++ n2_net_bandwidth_client.cc
@@ -30,31 +30,33 @@ int main(int argc, char *argv[]) {
     int server_fd = tcp_client_connect(argv[1], port);
 
     // set message size in bytes
-    int size = 1024 * 256;
+    int size = 1024 * 128;
 
-    // memset bytes to create string length size
-    memset(send_buf, '-', size);
-    send_buf[size] = '\0';
+    for (int size = 1024 * 1; size <= 1024 * 128; size = size * 2) {
+        // memset bytes to create string length size
+        memset(send_buf, '-', size);
+        send_buf[size] = '\0';
 
-    for (int i = 0; i < NUM_TRIAL; ++i) {
-        gettimeofday(&tval_start, NULL);
-        for (int j = 0; j < NUM_ITER; ++j) {
-            tcp_send(server_fd, send_buf);
-            // tcp_receive(server_fd, recv_buf);
+        for (int i = 0; i < NUM_TRIAL; ++i) {
+            gettimeofday(&tval_start, NULL);
+            for (int j = 0; j < NUM_ITER; ++j) {
+                tcp_send(server_fd, send_buf);
+                // tcp_receive(server_fd, recv_buf);
+            }
+            gettimeofday(&tval_end, NULL);
+            // convert time diff in usec directly to bandwidth
+            tval_diff = tval_diff_to_usec(tval_start, tval_end);
+            bytes_per_sec = (double) size * (double) NUM_ITER * (double) NUM_UNROLL
+                            / (double) tval_diff * (double) 1000000;
+            printf("tval_diff: %lu, bytes_per_sec: %f\n", tval_diff, bytes_per_sec);
+            // save it in time_trials
+            time_trials[i] = (unsigned long) bytes_per_sec;
+            printf("trail %d\n", i);
         }
-        gettimeofday(&tval_end, NULL);
-        // convert time diff in usec directly to bandwidth
-        tval_diff = tval_diff_to_usec(tval_start, tval_end);
-        bytes_per_sec = (double) size * (double) NUM_ITER * (double) NUM_UNROLL
-                        / (double) tval_diff * (double) 1000000;
-        printf("tval_diff: %lu, bytes_per_sec: %f\n", tval_diff, bytes_per_sec);
-        // save it in time_trials
-        time_trials[i] = (unsigned long) bytes_per_sec;
-        printf("trail %d\n", i);
+        std::cout << "#### size: " << size << std::endl;
+        // a HACK for outputing bandwith correctly, set both to 1
+        print_all_stats(time_trials, NUM_TRIAL, 1, 1);
     }
-    std::cout << "#### size: " << size << std::endl;
-    // a HACK for outputing the time correctly, set both to 1
-    print_all_stats(time_trials, NUM_TRIAL, 1, 1);
 
     tcp_shutdown_close(server_fd);
 
