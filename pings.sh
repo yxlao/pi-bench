34b16195fe1039f8d13a84b74a8a69de388d5498
Yixing Lao <linuxthink@gmail.com>
2016-03-14 05:56:17 -0700

    add pings command

---
+++ pings.sh
@@ -0,0 +1,23 @@
+ping localhost -s 13 -c 100 -i 0.2 -q
+ping localhost -s 14 -c 100 -i 0.2 -q
+ping localhost -s 16 -c 100 -i 0.2 -q
+ping localhost -s 20 -c 100 -i 0.2 -q
+ping localhost -s 28 -c 100 -i 0.2 -q
+ping localhost -s 44 -c 100 -i 0.2 -q
+ping localhost -s 76 -c 100 -i 0.2 -q
+ping localhost -s 140 -c 100 -i 0.2 -q
+ping localhost -s 268 -c 100 -i 0.2 -q
+ping localhost -s 524 -c 100 -i 0.2 -q
+ping localhost -s 1036 -c 100 -i 0.2 -q
+
+ping 192.168.0.18 -s 13 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 14 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 16 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 20 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 28 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 44 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 76 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 140 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 268 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 524 -c 100 -i 0.2 -q
+ping 192.168.0.18 -s 1036 -c 100 -i 0.2 -q
