0aa2a896fae08556379c9ac35b4edfc9355456ce
Yixing Lao <linuxthink@gmail.com>
2016-03-15 05:00:27 -0700

    single direction send, mbytes/s, reduce unroll

--- n2_net_bandwidth_client.cc
+++ n2_net_bandwidth_client.cc
@@ -5,9 +5,9 @@
 #include <sys/time.h>
 
 #define PORT "3490" // the port client will be connecting to
-#define NUM_TRIAL  50
+#define NUM_TRIAL  1
 #define NUM_ITER   2
-#define NUM_UNROLL 5
+#define NUM_UNROLL 2
 unsigned long time_trials[NUM_TRIAL];
 
 int main(int argc, char *argv[]) {
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
     // struct timeval tval_before, tval_after, tval_result;
     struct timeval tval_start, tval_end;
     unsigned long tval_diff;
-    double bytes_per_sec;
+    double mbytes_per_sec;
 
     // connect tcp
     int server_fd = tcp_client_connect(argv[1], port);
@@ -33,31 +33,23 @@ int main(int argc, char *argv[]) {
     // int size = 64 * 1024;
 
     // memset bytes to create string length size
-    for (int size = 1 * 1024; size < 512 * 1024; size = size * 2) {
+    for (int size = 1 * 1024; size < 64 * 1024 * 1024; size = size * 2) {
         memset(send_buf, '-', MAX_DATA_SIZE);
         send_buf[size] = '\0';
         for (int i = 0; i < NUM_TRIAL; ++i) {
             gettimeofday(&tval_start, NULL);
-            for (int j = 0; j < NUM_ITER / 2; ++j) {
+            for (int j = 0; j < NUM_ITER; ++j) {
                 tcp_send(server_fd, send_buf);
-                tcp_receive(server_fd, recv_buf);
-                tcp_send(server_fd, send_buf);
-                tcp_receive(server_fd, recv_buf);
-                tcp_send(server_fd, send_buf);
-                tcp_receive(server_fd, recv_buf);
-                tcp_send(server_fd, send_buf);
-                tcp_receive(server_fd, recv_buf);
-                tcp_send(server_fd, send_buf);
-                tcp_receive(server_fd, recv_buf);
+                // tcp_receive(server_fd, recv_buf);
             }
             gettimeofday(&tval_end, NULL);
             // convert time diff in usec directly to bandwidth
             tval_diff = tval_diff_to_usec(tval_start, tval_end);
-            bytes_per_sec = (double) size * (double) NUM_ITER * (double) NUM_UNROLL
-                            / (double) tval_diff * (double) 1000000;
-            // printf("tval_diff: %lu, bytes_per_sec: %f\n", tval_diff, bytes_per_sec);
+            mbytes_per_sec = (double) size * (double) NUM_ITER * (double) NUM_UNROLL
+                            / (double) tval_diff * (double) 1000000 / (double) 1048576;
+            // printf("tval_diff: %lu, mbytes_per_sec: %f\n", tval_diff, mbytes_per_sec);
             // save it in time_trials
-            time_trials[i] = (unsigned long) bytes_per_sec;
+            time_trials[i] = (unsigned long) mbytes_per_sec;
             // printf("trail %d\n", i);
         }
         std::cout << "#### size: " << size << std::endl;
