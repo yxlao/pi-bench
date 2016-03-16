511f4f61025c275bf199dcbbb8f417ca0022bf37
jiw241 <jiw241@ucsd.edu>
2016-03-15 19:18:53 -0700

    fix small errors

--- f2_file_seq_read.cc
+++ f2_file_seq_read.cc
@@ -9,10 +9,10 @@
 #include <string.h>
 #include "utils.h"
 // experiment repetitions
-#define NUM_TRIAL 1
+#define NUM_TRIAL 10
 #define NUM_UNROLL 1
 #define NUM_ITER 1
-unsigned long time_trials[NUM_TRIAL];
+unsigned long time_trials[8][NUM_TRIAL];
 using namespace std;
 
 #define MAX_INT_ARRAY_SIZE 16777216 / 2 // 32M Bytes
@@ -23,38 +23,46 @@ char buffer[BLOCK_SIZE] __attribute__((aligned(0x1000)));
 
 int main(int argc, char *argv[]) {
     //define variables
-    long long int size = 2 * 1024 * 1024;
+    long long int size;
     srand((unsigned int)time(NULL));
 
     char filename[128];
     int file1;
+    
+    for (int j = 0; j < NUM_TRIAL; j++) {
+      size = 2 * 1024 * 1024;
+      cout << j << endl;
+      //sequential access
+      for (int i = 0; i < 8; i++) {
+        cout << "size is " << size << endl;
+        sprintf (filename, "/home/pi/temp_%lld", size);
+        if (argc == 2) {
+          sprintf(filename, "/home/pi/nfs/temp_%lld", size);
+        }
 
-    //sequential access
-    for (int i = 0; i < 8; i++) {
+        file1 = open(filename, O_RDONLY | O_DIRECT);
 
-      sprintf (filename, "/home/pi/temp_%lld", size);
-      if (argc == 2) {
-        sprintf(filename, "/home/pi/nfs/temp_%lld", size);
-      }
-
-      file1 = open(filename, O_RDONLY | O_DIRECT);
-
-      RESET_CCNT;
-      GET_LOW_CCNT(time_start);
-      for (long long int k = 0; k < size; k += BLOCK_SIZE) {
+        RESET_CCNT;
+        GET_LOW_CCNT(time_start);
+        for (long long int k = 0; k < size; k += BLOCK_SIZE) {
           read(file1, buffer, BLOCK_SIZE);
-      }
-      GET_LOW_CCNT(time_end);
-      time_trials[0] = (time_end - time_start) * 64; 
-      cout << "seq, size: " << size << endl;
-      print_all_stats(time_trials, NUM_TRIAL, NUM_ITER, NUM_UNROLL);
+        }
+        GET_LOW_CCNT(time_end);
+        time_trials[i][j] = (time_end - time_start); 
+        close(file1);
 
-      cout << "" << endl;
-      close(file1);
+        size *= 2;
 
+      }//end of sequential access
+    }
+    
+    //output result
+    size = 2 * 1024 * 1024;
+    for (int k = 0; k < 8; k++) {
+      cout << "seq access, size is: " << size << endl;
+      print_all_stats(time_trials[k], NUM_TRIAL, NUM_ITER, NUM_UNROLL);
+      cout << " " << endl;
       size *= 2;
-
-    }//end of sequential access
-
+    }    
     return 0;
 }
